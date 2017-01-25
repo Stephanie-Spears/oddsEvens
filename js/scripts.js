19ae0347bc30afe56fc1f76cb1353e02410a5b7c
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-25 04:34:23 -0800

    add front-end display functions in js

--- js/scripts.js
+++ js/scripts.js
@@ -15,16 +15,27 @@ function makeArray(inputNumber, evensArray, oddsArray){
 
 
 
-
-
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-
+    $("#oddsList li,#evensList li").remove();
     var inputNumber = parseInt($("#input").val());
     var evensArray = [];
     var oddsArray = [];
     makeArray(inputNumber, evensArray, oddsArray);
-
+    for (var i=0; i < evensArray.length; i++){
+      // $("ul#evensList").append("<li class='animated bounceOutDown'>" + evensArray[i] + "</li>");
+      $("ul#evensList").append("<li>" + evensArray[i] + "</li>");
+      $("#evensList li").addClass('animated bounceInRight');
+    }
+    for (var x=0; x < oddsArray.length; x++){
+      // $("ul#oddsList").append("<li class='animated bounceOutDown'>" + oddsArray[x] + "</li>");
+      $("ul#oddsList").append("<li>" + oddsArray[x] + "</li>");
+      $("#oddsList li").addClass('animated bounceInLeft');
+    }
+    $("#reset").click(function(){
+      $("#evensList li,#oddsList li").removeClass();
+      $("#evensList li ,#oddsList li ").addClass('animated bounceOutDown');
+    });
   });
 });
